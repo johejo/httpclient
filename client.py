94426ea229c989db5f016d1af551569f4d730079
mitsuo <mitsuo_h@outlook.com>
2017-06-23 13:58:51 +0900

    test

--- client.py
+++ client.py
@@ -1,35 +1,47 @@
 import requests
 import sys
 
-chunk_size = int(sys.argv[1])
-url = sys.argv[2]
 
-byte = 0
-text = ''
-initial = 1
-size = 0
+def main():
+    chunk_size = int(sys.argv[1])
+    url = sys.argv[2]
 
-while True:
-    headers = {'Range': 'bytes={0}-{1}'.format(byte, byte + chunk_size - 1)}
-    byte += chunk_size
-    r = requests.get(url, headers=headers)
-    if r.status_code == 206:
-        if initial == 1:
-            cr = r.headers.get('Content-Range')
-            size = int(cr[cr.find('/') + 1:])
-            initial = 0
+    text = get_text(url, chunk_size)
 
-        text += r.text
+    print(text)
 
-        if byte >= size:
+
+def get_text(url, chunk_size):
+    text = ''
+    size = 0
+    initial = 1
+    byte = 0
+
+    while True:
+        headers = {'Range': 'bytes={0}-{1}'.format(byte, byte + chunk_size - 1)}
+        byte += chunk_size
+        r = requests.get(url, headers=headers)
+        if r.status_code == 206:
+            if initial == 1:
+                cr = r.headers.get('Content-Range')
+                size = int(cr[cr.find('/') + 1:])
+                initial = 0
+
+            text += r.text
+
+            if byte >= size:
+                break
+
+        elif r.status_code == 200:
+            text = r.text
             break
 
-    elif r.status_code == 200:
-        text = r.text
-        break
+        else:
+            print(r.status_code)
+            break
 
-    else:
-        print(r.status_code)
-        break
+    return text
 
-print(text)
+
+if __name__ == '__main__':
+    main()
