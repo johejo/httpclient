e2181032aa9fcdd0c8063f87796f7263ab4731c0
mitsuo <mitsuo_h@outlook.com>
2017-06-23 12:52:22 +0900

    test

--- client.py
+++ client.py
@@ -13,14 +13,19 @@ while True:
     headers = {'Range': 'bytes={0}-{1}'.format(byte, byte + chunk_size - 1)}
     byte += chunk_size
     r = requests.get(url, headers=headers)
-    if initial == 1:
-        cr = r.headers.get('Content-Range')
-        size = int(cr[cr.find('/') + 1:])
-        initial = 0
+    if r.status_code == 206:
+        if initial == 1:
+            cr = r.headers.get('Content-Range')
+            size = int(cr[cr.find('/') + 1:])
+            initial = 0
 
-    text += r.text
+        text += r.text
 
-    if byte >= size:
+        if byte >= size:
+            break
+
+    elif r.status_code == 200:
+        text = r.text
         break
 
 print(text)
