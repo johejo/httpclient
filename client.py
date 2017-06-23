1ff6c0a1b019839c48498f962abbb885ceb5785b
mitsuo <mitsuo_h@outlook.com>
2017-06-23 16:10:22 +0900

    test

--- client.py
+++ client.py
@@ -3,41 +3,47 @@ import sys
 
 
 def main():
-    chunk_size = int(sys.argv[1])
+    num = int(sys.argv[1])
     url = sys.argv[2]
 
-    text = get_text(url, chunk_size)
-
+    length = int(get_length(url))
+    text = get_text(url, num, length)
     print(text)
 
 
-def get_text(url, chunk_size):
-    text = ''
-    size = 0
-    initial = 1
-    byte = 0
+def get_length(url):
+    r = requests.head(url)
+    return r.headers['Content-Length']
 
-    while True:
+
+def get_text(url, num, length):
+    text = ''
+    byte = 0
+    chunk_size = int(length / num)
+    reminder = int(length % num)
+
+    if reminder != 0:
+        num += 1
+
+    for i in range(num):
+        if i == num:
+            chunk_size = reminder
+
         headers = {'Range': 'bytes={0}-{1}'.format(byte, byte + chunk_size - 1)}
+
         byte += chunk_size
+
         r = requests.get(url, headers=headers)
         if r.status_code == 206:
-            if initial == 1:
-                cr = r.headers.get('Content-Range')
-                size = int(cr[cr.find('/') + 1:])
-                initial = 0
 
             text += r.text
-
-            if byte >= size:
-                break
 
         elif r.status_code == 200:
             text = r.text
             break
 
         else:
-            print(r.status_code)
+            text = r.status_code
             break
 
     return text
